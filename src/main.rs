b39f5c94f0423c478d2978fb3c85126efe0fa4bd
marcelo <mhbcoelho99@gmail.com>
2023-10-08 20:22:58 -0300

    reading a file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 use std::env;
+use std::fs;
 
 fn main() {
     println!("mingrep in rust");
 
     let args: Vec<String> = env::args().collect();
 
+    let query: &str = &args[1];
+    let file_path: &str = &args[2];
+
+    let file_content = fs::read_to_string(file_path).expect("You cannot read this file");
+
+
     println!("{:#?}", args);
+
+    println!("Searching for: {}", query);
+    println!("In file: {}", file_path);
+
+
+    println!("File Content: \n{file_content}");
 }
