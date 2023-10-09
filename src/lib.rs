c4c872bc1f17aa95611c37b0fc0f28fc3148a4da
marcelo <mhbcoelho99@gmail.com>
2023-10-09 08:45:26 -0300

    add tests

---
+++ src/lib.rs
@@ -0,0 +1,65 @@
+use std::error::Error;
+use std::fs;
+
+
+pub struct Config{
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str>{
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query: &str = &args[1];
+        let file_path: &str = &args[2];
+    
+        Ok(Config{
+            query: query.to_string(),
+            file_path: file_path.to_string(),
+        })
+    }
+
+    pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+        let contents = fs::read_to_string(config.file_path)?;
+    
+        for line in search(&config.query, &contents) {
+            println!("{line}");
+        }
+    
+        Ok(())
+    }
+    
+}
+
+
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Running test in rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,8 @@
 use std::env;
-use std::fs;
 use std::process;
 
-struct Config{
-    _query: String,
-    file_path: String,
-}
+use mingrep::Config;
 
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str>{
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query: &str = &args[1];
-        let file_path: &str = &args[2];
-    
-        Ok(Config{
-            _query: query.to_string(),
-            file_path: file_path.to_string(),
-        })
-    }
-    
-}
 
 fn main() {
     println!("mingrep in rust");
@@ -33,8 +14,7 @@ fn main() {
         process::exit(1);
     });
 
-    let file_content = fs::read_to_string(config.file_path).expect("You cannot read this file");
+    Config::run(config).expect("You cannot read this file");
 
-    println!("File Content: \n{file_content}");
 }
 
