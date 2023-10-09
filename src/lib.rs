cf1095ee404dfcdb911a7a86f353a2c91576e215
marcelo <mhbcoelho99@gmail.com>
2023-10-09 10:13:00 -0300

    adding case sensitive test

--- src/lib.rs
+++ src/lib.rs
@@ -57,9 +57,24 @@ mod tests {
         let contents = "\
 Running test in rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
 
