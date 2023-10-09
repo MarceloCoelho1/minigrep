7e4965c0b2e0b384cd345e85137a8cd94cd51c3d
marcelo <mhbcoelho99@gmail.com>
2023-10-08 21:17:17 -0300

    adding a constructor for Config

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::fs;
-
+use std::process;
 
 struct Config{
     _query: String,
@@ -8,17 +8,17 @@ struct Config{
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config{
+    fn build(args: &[String]) -> Result<Config, &'static str>{
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
         let query: &str = &args[1];
         let file_path: &str = &args[2];
     
-        Config{
+        Ok(Config{
             _query: query.to_string(),
             file_path: file_path.to_string(),
-        }
+        })
     }
     
 }
@@ -28,7 +28,10 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    let config: Config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     let file_content = fs::read_to_string(config.file_path).expect("You cannot read this file");
 
