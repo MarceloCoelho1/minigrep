d5b4f25cbe86a7e2b72c8608951ad9acdeabcb9c
marcelo <mhbcoelho99@gmail.com>
2023-10-08 21:09:55 -0300

    struct Config

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,37 @@
 use std::env;
 use std::fs;
 
+
+struct Config{
+    _query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config{
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+        let query: &str = &args[1];
+        let file_path: &str = &args[2];
+    
+        Config{
+            _query: query.to_string(),
+            file_path: file_path.to_string(),
+        }
+    }
+    
+}
+
 fn main() {
     println!("mingrep in rust");
 
     let args: Vec<String> = env::args().collect();
 
-    let query: &str = &args[1];
-    let file_path: &str = &args[2];
+    let config: Config = Config::new(&args);
 
-    let file_content = fs::read_to_string(file_path).expect("You cannot read this file");
-
-
-    println!("{:#?}", args);
-
-    println!("Searching for: {}", query);
-    println!("In file: {}", file_path);
-
+    let file_content = fs::read_to_string(config.file_path).expect("You cannot read this file");
 
     println!("File Content: \n{file_content}");
 }
+
